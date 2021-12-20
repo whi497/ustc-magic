11b25478c9581b62e4b834b8960b6746b58c0492
whi497 <1624745389@qq.com>
2021-12-20 22:20:44 +0800

    time

--- lastwork/topmodule.v
+++ lastwork/topmodule.v
@@ -19,21 +19,21 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module topmodule (
-    input clk,rst,button,
-    input [7:1] sw,
+    input clk,button,
+    input rx,
+    input [7:7] sw,
     output [7:0] led,
+    output tx,
     output reg [3:0] out,
     output reg [2:0] selsct
 );
-
+    wire rst;
     wire clk_n;
     wire button_n;
     wire button_edge;
     wire rst_n,rst_edge;
     jtrclr clr(.clk(clk),.button(button),.button_n(button_n));
     signal_edge cedge(.clk(clk),.button(button_n),.button_edge(button_edge));
-    jtrclr clr1(.clk(clk),.button(rst),.button_n(rst_n));
-    signal_edge cedge1(.clk(clk),.button(rst_n),.button_edge(rst_edge));
     clk_wiz_0 clk_wiz_0_insrt(.clk_in1(clk),.clk_out1(clk_n),.reset(rst),.locked(locked));
 
     parameter IDLE  = 4'd0;
@@ -49,74 +49,95 @@ module topmodule (
     parameter E = 2'd3;
 
     reg usercheck;
+    reg er;
     reg [1:0] error_cout;
     reg [3:0] curr_state;
     reg [3:0] next_state;
 
+    wire is_reset_cmd;
     wire is_check_cmd;
-    assign is_check_cmd=rst_edge;
+    wire is_start_cmd;
+    wire is_setcl_cmd;
+    wire is_exit_cmd;
+    wire is_shutdown_cmd;
     reg [1:0] inputcout;
     reg [1:0] pa_custate;
     reg [1:0] pa_nestate;
-    reg [6:0] password1;
-    reg [6:0] password2;
-    reg [6:0] password3;
+    reg [7:0] password1;
+    reg [7:0] password2;
+    reg [7:0] password3;
 
+    wire wr_ry;
     reg [19:0] cout;
-    reg [3:0] clock1,clock2,clock3;
-    reg clockfinished;
+    reg [3:0] clock [6:0];
+    wire [3:0] cltemp [6:1];
+    wire clockfinished;
     reg clockstart;
     wire pulse_10hz;
 
     reg [3:0] hexdisplay [7:0];
 
+    iouart IO(.clk(clk),.rst(rst),.rx(rx),.tx(tx),.er(er),
+            .is_check_cmd(is_check_cmd),.is_start_cmd(is_start_cmd),
+            .is_reset_cmd(is_reset_cmd),.is_setcl_cmd(is_setcl_cmd),
+            .is_exit_cmd(is_exit_cmd),.is_shutdown_cmd(is_shutdown_cmd),
+            .wr_ry(wr_ry),
+            .clock1(cltemp[1]),.clock2(cltemp[2]),.clock3(cltemp[3]),
+            .clock4(cltemp[4]),.clock5(cltemp[5]),.clock6(cltemp[6]));   
+    assign rst=is_reset_cmd||is_shutdown_cmd;
     always@(posedge clk or posedge rst)
     begin
-        if(rst)
+        if(rst)begin
             curr_state  <= IDLE;
+            next_state  <= IDLE;
+        end
         else
             curr_state  <= next_state;
     end
     always@(*)
     begin
         case(curr_state)
-        IDLE: begin if(button_edge) next_state=LOCKED; else next_state=IDLE; end
+        IDLE: begin if(is_start_cmd) next_state=LOCKED; else next_state=IDLE; end
         LOCKED: begin
-            if(button_edge) begin 
-                if(usercheck) 
-                    next_state=WORK; 
+            if(is_check_cmd) begin 
+                if(usercheck) next_state=WORK; 
                 else begin 
-                    if(error_cout==2'd2) 
-                        next_state=ERRORLOCKED;
-                    else 
-                        next_state=ERROR;
+                    if(error_cout==2'd2) next_state=ERRORLOCKED;
+                    else next_state=ERROR;
                 end
             end
             else next_state=LOCKED;
         end
         ERROR:begin
             next_state = LOCKED;
-            // if(finished) next_state=LOCKED;
-            // else next_state=ERROR;
         end
         ERRORLOCKED: begin
-            if(clockfinished) 
-                next_state=LOCKED;
-            else 
-                next_state=ERRORLOCKED;
+            if(clockfinished) next_state=LOCKED;
+            else next_state=ERRORLOCKED;
         end
         WORK: begin
-            if(rst) 
-                next_state=IDLE;
-            if(sw[1]) 
-                next_state=CLOCK; 
-            else 
-                next_state=WORK;
+            if(is_exit_cmd) next_state=LOCKED;
+            else if(is_setcl_cmd) next_state=CLOCK; 
+            else next_state=WORK;
+        end
+        CLOCK:begin 
+            if(is_exit_cmd) next_state=WORK;
+            else next_state=CLOCK;
         end
         default: next_state=IDLE; 
         endcase
     end
 
+    always@(posedge clk)
+    begin
+        if(rst)er<=0;
+        else 
+        begin 
+            if(curr_state==LOCKED&&button_edge&&~usercheck)
+            er<=1;
+            else er<=0;
+        end
+    end 
 
     always@(posedge clk or posedge rst)begin
         if(rst)begin
@@ -124,7 +145,7 @@ module topmodule (
             pa_custate<=A;
         end
         else begin
-            if(pa_custate==C&&next_state==A) usercheck<=1;
+            if(pa_custate==C&&pa_nestate==A) usercheck<=1;
             else if(button_edge) usercheck<=0;
             pa_custate<=pa_nestate;
         end
@@ -133,7 +154,7 @@ module topmodule (
         if(rst)
             inputcout <= 2'b0;
         else begin
-            if(is_check_cmd)
+            if(button_edge)
             begin 
                 inputcout<=inputcout+2'b1;
             end
@@ -141,28 +162,28 @@ module topmodule (
     end
     always@(posedge clk or posedge rst)begin
         if(rst)error_cout<=2'b0;
-        else if(button_edge&&(~usercheck))
+        else if(is_check_cmd&&(~usercheck))
         begin
             if(error_cout==2'd2)error_cout<=2'b0;
             else error_cout<=error_cout+2'b1;
         end
     end
     always@(*)begin
-        if(button_edge) pa_nestate=A;
-        if(is_check_cmd)begin
+        if(is_check_cmd) pa_nestate=A;
+        if(button_edge&&curr_state==LOCKED)begin
             case(pa_custate)
             A: begin 
-                if(sw[7:1]==password1)
+                if(sw==password1)
                     pa_nestate=B;
                 else pa_nestate=E;
             end
             B: begin
-                if(sw[7:1]==password2)
+                if(sw==password2)
                     pa_nestate=C;
                 else pa_nestate=E;
             end
             C: begin
-                if(sw[7:1]==password3)
+                if(sw==password3)
                     pa_nestate=A;
                 else pa_nestate=E;
             end
@@ -171,6 +192,7 @@ module topmodule (
                     pa_nestate=A;
                 else pa_nestate=E;
             end
+            default: pa_nestate=A;
             endcase
         end
     end
@@ -178,12 +200,10 @@ module topmodule (
 
     always@(posedge clk_n or posedge rst)                     
     begin
-        if(rst)
-        begin
+        if(rst)begin
             cout<=20'b0;
         end
-        else
-        begin
+        else begin
             if(cout>=20'd999999)
             cout<=20'b0;
             else cout<=cout+20'b1;
@@ -192,15 +212,21 @@ module topmodule (
     assign pulse_10hz=(cout==20'd999999);
     always@(posedge clk or posedge rst)
     begin
-        if(rst || clockfinished) begin
-            clock1 <= 4'd0;
-            clock2 <= 4'd0;
-            clock3 <= 4'd0;
-            clockfinished <=1'b0;
+        if(rst || clockfinished || (is_exit_cmd&&(curr_state==WORK||curr_state==CLOCK))) begin
+            clock[0]<=4'b0;clock[1]<=4'b0;clock[2]<=4'b0;
+            clock[3]<=4'b0;clock[4]<=4'b0;clock[5]<=4'b0;
+            clock[6]<=4'b0;clock[7]<=4'b0;
             clockstart <= 1'b0;
         end
+        else if(curr_state==CLOCK && button_edge)begin
+            clockstart <= 1'b1;
+        end
+        else if(wr_ry)begin
+            clock[6]<=cltemp[1];clock[5]<=cltemp[2];clock[4]<=cltemp[3];
+            clock[3]<=cltemp[4];clock[2]<=cltemp[5];clock[1]<=cltemp[6];
+        end
         else if(error_cout==2'd2)begin
-            clock1 = 4'd6;
+            clock[2] = 4'd6;
             clockstart <=1'b1;
         end
         else
@@ -209,37 +235,56 @@ module topmodule (
                 begin
                     if(pulse_10hz)
                     begin
-                        if(clock3==4'b0)begin
-                            clock3<=4'd9;
-                            if(clock2==4'b0)begin
-                                clock2<= 4'd9;
-                                if(clock1==4'b0) clockfinished<=1'b1;
-                                else clock1<=clock1-4'b1;
+                        if(clock[0]==4'b0)begin
+                            clock[0]<=4'd9;
+                            if(clock[1]==4'b0)begin
+                                clock[1]<= 4'd9;
+                                if(clock[2]==4'b0)begin
+                                    clock[2]<= 4'd6;
+                                    if(clock[3]==4'b0)begin
+                                        clock[3]<= 4'd9;
+                                        if(clock[4]==4'b0)begin
+                                            clock[4]<=4'd6;
+                                            if(clock[5]==4'b0)begin
+                                                clock[5]<= 4'd9;
+                                                clock[6]<= clock[6]-4'b1;
+                                            end
+                                            else clock[5]<=clock[5]-4'b1;
+                                        end
+                                        else clock[4]<= clock[4]-4'b1;
+                                    end
+                                    else clock[3]<=clock[3]-4'b1;
+                                end
+                                else clock[2]<=clock[2]-4'b1;
                             end
-                            else clock2<=clock2-4'b1;
+                            else clock[1]<=clock[1]-4'b1;
                         end
-                        else clock3<=clock3-4'b1;
+                        else clock[0]<=clock[0]-4'b1;
                     end
                 end
             end
     end
+    assign clockfinished = ((clockstart&&clock[0]&&clock[1]&&clock[2]&&(curr_state==ERRORLOCKED))
+                            ||(clockstart&&clock[0]&&clock[1]&&clock[2]&&clock[3]&&clock[4]&&
+                            clock[5]&&clock[6]&&(curr_state==CLOCK)));
 
     reg [2:0] enable;
     always@(posedge clk_n)
     begin
-        enable=enable+1'b1;
+        if(rst||curr_state==IDLE)enable<=3'b0;
+        else enable=enable+3'b1;
     end
     always@(*)
     begin
         case(enable)
-        3'd1: begin out=hexdisplay[0];selsct=3'd0; end
+        3'd0: begin out=hexdisplay[0];selsct=3'd0; end
         3'd1: begin out=hexdisplay[1];selsct=3'd1; end
-        3'd1: begin out=hexdisplay[2];selsct=3'd2; end
-        3'd1: begin out=hexdisplay[3];selsct=3'd3; end
-        3'd1: begin out=hexdisplay[4];selsct=3'd4; end
-        3'd1: begin out=hexdisplay[5];selsct=3'd5; end
-        3'd1: begin out=hexdisplay[6];selsct=3'd6; end
-        3'd1: begin out=hexdisplay[7];selsct=3'd7; end
+        3'd2: begin out=hexdisplay[2];selsct=3'd2; end
+        3'd3: begin out=hexdisplay[3];selsct=3'd3; end
+        3'd4: begin out=hexdisplay[4];selsct=3'd4; end
+        3'd5: begin out=hexdisplay[5];selsct=3'd5; end
+        3'd6: begin out=hexdisplay[6];selsct=3'd6; end
+        3'd7: begin out=hexdisplay[7];selsct=3'd7; end
         default:begin out=hexdisplay[0];selsct=3'b0; end
         endcase
     end
@@ -248,9 +293,13 @@ module topmodule (
     begin
         if(curr_state==ERRORLOCKED)
         begin
-            hexdisplay[0]<=clock3;
-            hexdisplay[1]<=clock2;
-            hexdisplay[2]<=clock1;
+            hexdisplay[0]<=clock[0];
+            hexdisplay[1]<=clock[1];
+            hexdisplay[2]<=clock[2];
+            hexdisplay[3]<=clock[3];
+            hexdisplay[4]<=clock[4];
+            hexdisplay[5]<=clock[5];
+            hexdisplay[6]<=clock[6];
         end
         else begin
             hexdisplay[0]<=0;
@@ -267,9 +316,9 @@ module topmodule (
     assign led={2'b0,pa_custate,curr_state};
 
     initial begin
-        usercheck=0;error_cout=0;
+        usercheck=0;error_cout=0;er=0;
         password1=0;password2=0;password3=0;
-        clockfinished=0; clockstart=0;
+        clockstart=0;
     end
 
 endmodule
--- lastwork/work.xdc
+++ lastwork/work.xdc
@@ -8,15 +8,18 @@ set_property -dict { PACKAGE_PIN E3    IOSTANDARD LVCMOS33 } [get_ports { clk }]
 #create_clock -add -name sys_clk_pin -period 10.00 -waveform {0 5} [get_ports {CLK100MHZ}];
 ## FPGAOL BUTTON & SOFT_CLOCK
 set_property -dict { PACKAGE_PIN B18   IOSTANDARD LVCMOS33 } [get_ports { button }];
+#uart
+set_property -dict { PACKAGE_PIN C4    IOSTANDARD LVCMOS33 } [get_ports { rx }];
+set_property -dict { PACKAGE_PIN D4    IOSTANDARD LVCMOS33 } [get_ports { tx }]; 
 ## FPGAOL LED (signle-digit-SEGPLAY)
-set_property -dict { PACKAGE_PIN C17   IOSTANDARD LVCMOS33 } [get_ports { led[7] }];
-set_property -dict { PACKAGE_PIN D18   IOSTANDARD LVCMOS33 } [get_ports { led[6] }];
-set_property -dict { PACKAGE_PIN E18   IOSTANDARD LVCMOS33 } [get_ports { led[5] }];
-set_property -dict { PACKAGE_PIN G17   IOSTANDARD LVCMOS33 } [get_ports { led[4] }];
-set_property -dict { PACKAGE_PIN D17   IOSTANDARD LVCMOS33 } [get_ports { led[3] }];
-set_property -dict { PACKAGE_PIN E17   IOSTANDARD LVCMOS33 } [get_ports { led[2] }];
-set_property -dict { PACKAGE_PIN F18   IOSTANDARD LVCMOS33 } [get_ports { led[1] }];
-set_property -dict { PACKAGE_PIN G18   IOSTANDARD LVCMOS33 } [get_ports { led[0] }];
+set_property -dict { PACKAGE_PIN C17   IOSTANDARD LVCMOS33 } [get_ports { led[0] }];
+set_property -dict { PACKAGE_PIN D18   IOSTANDARD LVCMOS33 } [get_ports { led[1] }];
+set_property -dict { PACKAGE_PIN E18   IOSTANDARD LVCMOS33 } [get_ports { led[2] }];
+set_property -dict { PACKAGE_PIN G17   IOSTANDARD LVCMOS33 } [get_ports { led[3] }];
+set_property -dict { PACKAGE_PIN D17   IOSTANDARD LVCMOS33 } [get_ports { led[4] }];
+set_property -dict { PACKAGE_PIN E17   IOSTANDARD LVCMOS33 } [get_ports { led[5] }];
+set_property -dict { PACKAGE_PIN F18   IOSTANDARD LVCMOS33 } [get_ports { led[6] }];
+set_property -dict { PACKAGE_PIN G18   IOSTANDARD LVCMOS33 } [get_ports { led[7] }];
 
 ## FPGAOL SWITCH
 set_property -dict { PACKAGE_PIN D14   IOSTANDARD LVCMOS33 } [get_ports { rst }];
